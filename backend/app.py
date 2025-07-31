64a6331c7dae606fbfac75439a4e324683ef1b41
SrRenho <40799539+SrRenho@users.noreply.github.com>
2025-07-31 01:59:45 -0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -11,15 +11,15 @@ CORS(app)
 def query_api():
     data = request.get_json()
     user_input = data.get("user_input", "")
-    response = query.generate_response(user_input)
+    response = "probando probando 1 2 3 " #query.generate_response(user_input)
     return response
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=True)
 
-    database.generate_data_store()
-    query.__init__()
+ #   database.generate_data_store()
+ #   query.__init__()
     print("Page ready to use!")
 
 
