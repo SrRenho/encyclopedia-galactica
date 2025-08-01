ef62cd3b608c17e2e10bdbbb2b0940c30ca6bf9e
SrRenho <40799539+SrRenho@users.noreply.github.com>
2025-08-01 20:47:05 -0300

    heavy loging

--- backend/query.py
+++ backend/query.py
@@ -9,25 +9,34 @@ EMBEDDING_MODEL_NAME = "sentence-transformers/paraphrase-MiniLM-L3-v2"
 TOGETHER_MODEL_NAME = "mistralai/Mixtral-8x7B-Instruct-v0.1"
 
 def generate_response(query_text):
-    embedding_function = SentenceTransformerEmbeddings(model_name=EMBEDDING_MODEL_NAME)
-    db = Chroma(persist_directory=CHROMA_PATH, embedding_function=embedding_function)
+    print("received query")
+    db = Chroma(persist_directory=CHROMA_PATH, embedding_function=SentenceTransformerEmbeddings(model_name=EMBEDDING_MODEL_NAME))
+    print("db object created)
 
     results = db.similarity_search(query_text, k=2)
+    print("db searched")
     if not results:
+        print("no results")
         return "No relevant documents found."
 
+    print("creating context text from found results")
     context_text = "\n\n---\n\n".join([doc.page_content for doc in results])
 
+    print("creating prompt")
     prompt = (
         "Use the following sources to answer the question:\n\n"
         f"{context_text}\n\n"
         "Question: {query_text}."
     )
 
+    print("creating llm object)
     llm = Together(model=TOGETHER_MODEL_NAME, temperature=0.7, max_tokens=200)
+    print("querying llm")
     response_text = llm.invoke(prompt)
 
+    print("returning response")
     return f"{response_text}\n\nSources:\n{context_text}"
+
 
 
 
