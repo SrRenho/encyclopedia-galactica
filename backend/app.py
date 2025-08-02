cecd417baccf138012980f0f5be3fb7d8e90ac5b
SrRenho <40799539+SrRenho@users.noreply.github.com>
2025-08-01 21:23:08 -0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -7,12 +7,14 @@ app = Flask(__name__)
 CORS(app)
 
 @app.route("/query", methods=["POST"])
-def query_api():        
+def query_api():
+    print("query recibida")
     data = request.get_json()
     user_input = data.get("user_input", "")
-    response = query.generate_response(user_input)
+    print("user input recibido")
+    response = "probando 1 2 3 " #query.generate_response(user_input)
     return response
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, debug=True)
