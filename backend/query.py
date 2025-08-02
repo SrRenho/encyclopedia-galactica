a3642ec126decaf86db4b5b63ab39da0a01e980a
SrRenho <40799539+SrRenho@users.noreply.github.com>
2025-08-02 00:25:36 -0300

    Update query.py

--- backend/query.py
+++ backend/query.py
@@ -2,40 +2,42 @@ from langchain.prompts import ChatPromptTemplate
 from langchain_community.embeddings import SentenceTransformerEmbeddings
 from langchain_chroma import Chroma
 from langchain_together import Together
+import logging
 
 CHROMA_PATH = "chroma"
 
 EMBEDDING_MODEL_NAME = "sentence-transformers/paraphrase-MiniLM-L3-v2"
 TOGETHER_MODEL_NAME = "mistralai/Mixtral-8x7B-Instruct-v0.1"
 
-def generate_response(query_text):
-    print("received query")
+def generate_response(query_text, app):
+    app.logger.info("received query")
     db = Chroma(persist_directory=CHROMA_PATH, embedding_function=SentenceTransformerEmbeddings(model_name=EMBEDDING_MODEL_NAME))
-    print("db object created")
+    app.logger.info("db object created")
 
     results = db.similarity_search(query_text, k=2)
-    print("db searched")
+    app.logger.info("db searched")
     if not results:
-        print("no results")
+        app.logger.info("no results")
         return "No relevant documents found."
 
-    print("creating context text from found results")
+    app.logger.info("creating context text from found results")
     context_text = "\n\n---\n\n".join([doc.page_content for doc in results])
 
-    print("creating prompt")
+    app.logger.info("creating prompt")
     prompt = (
         "Use the following sources to answer the question:\n\n"
         f"{context_text}\n\n"
         "Question: {query_text}."
     )
 
-    print("creating llm object")
+    app.logger.info("creating llm object")
     llm = Together(model=TOGETHER_MODEL_NAME, temperature=0.7, max_tokens=200)
-    print("querying llm")
+    app.logger.info("querying llm")
     response_text = llm.invoke(prompt)
 
-    print("returning response")
+    app.logger.info("returning response")
     return f"{response_text}\n\nSources:\n{context_text}"
+
 
 
 
